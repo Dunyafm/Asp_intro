fa925f5879e676fa44b36bce04920b9d30dddf8c
Dunyafm <dunyafm@code.edu.az>
2022-03-02 15:34:18 -0800

    Intro-pratice

---
+++ AspIntro/AspIntro/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspIntro.Controller
+{
+    public class HomeController : Controller
+    {    
+
+        //public string Index()
+        //{
+        //    return "Hello";
+        //}
+    }
+
+}
